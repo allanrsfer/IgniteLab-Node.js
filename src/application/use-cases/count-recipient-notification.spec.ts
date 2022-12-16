18096340428c9ffe2721390926b43a178b3c6eef
Allan Rieri <allanrieri@gmail.com>
2022-12-16 16:27:05 -0300

    finish project

---
+++ src/application/use-cases/count-recipient-notification.spec.ts
@@ -0,0 +1,31 @@
+import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repositories";
+import { CountRecipientNotification } from "./count-recipient-notification";
+import { makeNotification } from "../../../test/factories/notification-factory";
+
+
+describe('Count recipients notification', () => {
+  it('should be able to count recipient notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotification = new CountRecipientNotification(
+      notificationsRepository
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' })
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' })
+    );;
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' })
+    );;
+
+    const { count } = await countRecipientNotification.execute({
+      recipientId: 'recipient-1'
+    });
+
+    expect(count).toEqual(2);
+  })
+})
